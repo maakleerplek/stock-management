363ff2aca42361b42fb7fde2048ecbe9b8d81781
Eggmansmile <ruben.ryckaert89@gmail.com>
2025-08-19 18:33:58 +0200

    Added basic ssl

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+  },
 })
